1145d54abd7a7dbb5cd434fca0fd53b028c6c544
dom <dom.barga@gmail.com>
2026-04-15 23:43:16 +0800

    added test

---
+++ app/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPingCommand(t *testing.T) {
+	arg := "*1\r\n$4\r\nPING\r\n"
+
+	actual := handleCommandResponse([]byte(arg))
+	expected := "+PONG\r\n"
+
+	// fmt.Println(actual, expected)
+	if actual != expected {
+		t.Errorf("expected %s, got %s", expected, actual)
+	}
+}
+
+func TestEchoCommand(t *testing.T) {
+	arg := "*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n"
+
+	actual := handleCommandResponse([]byte(arg))
+	expected := "$3\r\nhey\r\n"
+
+	// fmt.Println(actual, expected)
+	if actual != expected {
+		t.Errorf("expected %s, got %s", expected, actual)
+	}
+}
