60eea5fe5db270c3d95bca1b03f3f404781a4571
dom <dom.barga@gmail.com>
2026-04-17 03:55:41 +0800

    SET and GET redis commands

--- app/main.go
+++ app/main.go
@@ -10,6 +10,7 @@ import (
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+var rcache = map[string]any{}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -37,9 +38,12 @@ func main() {
 			defer conn.Close()
 			for {
 				b := make([]byte, 1024)
-				conn.Read(b)
+				n, err := conn.Read(b)
+				if err != nil {
+					return
+				}
 
-				resp := handleIncomingCommand(b)
+				resp := handleIncomingCommand(b[:n])
 				fmt.Println("resp", resp)
 				conn.Write([]byte(resp))
 			}
@@ -58,6 +62,7 @@ func getResponse(_input []byte) string {
 	// The input is a Redis RESP Array of bulk string,
 	// that is, *<number-of-elements>\r\n<element-1>...<element-n>.
 	input := string(_input)
+	fmt.Printf("input here %q \n", input)
 
 	respInput := strings.Split(input, "\r\n")
 	// fmt.Printf("zxc %d %q\n", len(respInput), respInput)
@@ -71,6 +76,11 @@ func getResponse(_input []byte) string {
 	case "ECHO":
 		// ECHO argument is in 4th index.
 		return handleEchoCmd(respInput[4])
+	case "SET":
+		return handleSetCommand(respInput)
+	case "GET":
+		// ECHO argument is in 4th index.
+		return handleGetCommand(respInput)
 	}
 
 	return "not a valid command"
@@ -87,4 +97,43 @@ func getCommand(input []string) string {
 		return input[2]
 	}
 	return ""
+}
+
+func handleSetCommand(respInput []string) string {
+	var currKey string
+	// var val any
+
+	// Start loop after the command index.
+	for _, r := range respInput[3 : len(respInput)-1] {
+		// fmt.Println("kv", currKey, rcache, r)
+		if string(r[0]) != "$" {
+			if currKey == "" {
+				currKey = r
+				rcache[r] = nil
+			}
+
+			v, _ := rcache[currKey]
+			// fmt.Println("setting", v, r, currKey)
+			if v == nil && r != currKey && string(r[0]) != "$" {
+				rcache[currKey] = r
+				currKey = ""
+			}
+		}
+
+		// val = nil
+		// fmt.Println("-------------")
+	}
+
+	fmt.Println("rcache", rcache)
+
+	return "+OK\r\n"
+}
+
+func handleGetCommand(respInput []string) string {
+	val, ok := rcache[respInput[4]]
+	if !ok {
+		return "$-1\r\n"
+	}
+
+	return fmt.Sprintf("$%d\r\n%s\r\n", len(val.(string)), val)
 }
--- app/main_test.go
+++ app/main_test.go
@@ -27,3 +27,29 @@ func TestEchoCommand(t *testing.T) {
 		t.Errorf("expected %s, got %s", expected, actual)
 	}
 }
+
+func TestSetCommandResponse(t *testing.T) {
+	arg := "*2\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n$3\r\nwar\r\n$3\r\nsad\r\n"
+
+	actual := handleIncomingCommand([]byte(arg))
+	expected := "+OK\r\n"
+
+	// fmt.Println(actual, expected)
+	if actual != expected {
+		t.Errorf("expected %s, got %s", expected, actual)
+	}
+}
+
+// func TestSetKeyValueCommand(t *testing.T) {
+// 	arg := "*2\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n$3\r\nwar\r\n$3\r\nsad\r\n"
+
+// 	handleIncomingCommand([]byte(arg))
+// 	actual := rcache["foo"]
+// 	expected := "bar"
+// 	// fmt.Println("rcache", rcache)
+// 	// fmt.Println(actual, expected)
+
+// 	if actual != expected {
+// 		t.Errorf("expected %s, got %s", expected, actual)
+// 	}
+// }
